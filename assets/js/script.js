487aa58f77dbb041e446019d5e7ef7c5830880dd
JacobYaws <Jacob.Yaws@du.edu>
2022-11-03 17:39:19 -0600

    Updated HTML, CSS, and JS files to get a somewhat working product.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+const beginningText = document.getElementById('start-text');
 
 let shuffledQuestions, currentQuestionIndex;
 
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+})
 
 var score = 0;
 
@@ -17,6 +23,7 @@ function startGame() {
     console.log('Started');
     startButton.classList.add('hide');
     questionContainerElement.classList.remove('hide');
+    beginningText.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     setNextQuestion();
@@ -49,9 +56,34 @@ function resetState() {
     }
 }
 
-function selectAnswer() {
-
+function selectAnswer(event) {
+    const selectedButton = event.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
+    }
 };
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+};
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
 
 const questions = [
     {
@@ -61,6 +93,15 @@ const questions = [
            { text: '16', correct: false},
            { text: '8', correct: true},
            { text: '44', correct: false},
-        ]
+        ],
+    },
+    {
+        question: 'What is 2 x 4?',
+        answers: [
+           { text: '8', correct: true},
+           { text: '6', correct: false},
+           { text: '10', correct: false},
+           { text: '24', correct: false},
+        ],
     }
 ];
