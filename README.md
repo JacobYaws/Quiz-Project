da790e1eaeb39c495f8cdb87524c2a7c2890ce87
JacobYaws <Jacob.Yaws@du.edu>
2022-11-07 23:46:57 -0700

    Added the final comments to the js file and added in the quiz questions.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Quiz-Project
+
+## Description
+
+For this project, I was tasked with creating a quiz webpage using javascript.
+
+
+## Table of Contents
+
+[Overview](#overview)
+[Screenshots](#screenshots)
+[Links](#links)
+[References](#references)
+
+
+## Overview
+-In this project, I created a webpage that takes the user through a quiz of four general knowledge questions. A flexbox container was made to display the question and answer boxes for the user to click through.
+-A timer was added to the page that would count down as the quiz was in progress. If the wrong answer was selected, the timer was reduced by 15 seconds. If the timer reached 0 before the end of the quiz, the user would be taken to the end of the quiz. The page also reacted to whether an answer was selected correctly or not. If the answer was correct, the background would change to green and, if incorrect, the page would change to red.
+-At the end of the quiz, the user is shown their score and is also able to enter their initials for the highscores page. 
+-The highscores page will show the score of each unique initial entered. From this page, the user can restart the quiz with time fully restored and their score set back to 0.
+
+## Screenshots
+  
+  <img src="">
+  
+  <img src="">
+
+
+## Useful Links  
+
+https://github.com/JacobYaws/Quiz-Project
+ 
+    -To download, navigate to the repository and click on the green 'Code' button. Copy the ssh link and clone it in a terminal by using 'git clone git@github.com:JacobYaws/Quiz-Project.git'
+
+ 
+Link to the live website: https://jacobyaws.github.io/Quiz-Project/
+
+## References
+-For help with creating the quiz: https://www.youtube.com/watch?v=riDzcEQbX6k&t=1021s
+-For help with storing the highscores: https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+-For help with the quiz questions: https://www.opinionstage.com/blog/trivia-questions/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 // These are all of the global declarations for the script. These will be used for the functions further down. 
 
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
+const startButtonElement = document.getElementById('start-btn');
+const nextButtonElement = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 const beginningTextElement = document.getElementById('start-text');
 const containerElement = document.getElementById('container');
 const endTextElement = document.getElementById('end-screen');
-const submitButton = document.getElementById('submit-btn');
-const restartButton = document.getElementById('restart')
-const endHeader = document.getElementById('end-header');
-const scores = document.getElementById('high-scores');
-const finalScore = document.getElementById('final-score');
-const timer = document.getElementById('timer')
-var inputText = document.getElementById('inputText');
-const ul = document.getElementById('ul');
+const submitButtonElement = document.getElementById('submit-btn');
+const restartButtonElement = document.getElementById('restart')
+const endHeaderElement = document.getElementById('end-header');
+const scoresElement = document.getElementById('high-scores');
+const finalScoreElement = document.getElementById('final-score');
+const timerElement = document.getElementById('timer')
+var inputTextElement = document.getElementById('inputText');
+const ulElement = document.getElementById('ul');
 var score
 var time
 let shuffledQuestions, currentQuestionIndex;
@@ -24,7 +24,7 @@ let gameEnd = false;
 
 // This event listeners is used for the start button and, when clicked, will call the startGame function.  
 
-startButton.addEventListener('click', startGame);
+startButtonElement.addEventListener('click', startGame);
 
 
 
@@ -52,7 +52,7 @@ function setTime() {
 // be set to 0 so if the user takes the quiz multiple times, they will not be able to add on to any scores from previous attempts.
 
 function startGame() {
-    startButton.classList.add('hide');
+    startButtonElement.classList.add('hide');
     questionContainerElement.classList.remove('hide');
     beginningTextElement.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
@@ -72,8 +72,8 @@ function setNextQuestion() {
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 };
 
-// This function is used to show the question in the container. It uses the questions array (further below) to change the question and answers each time the quiz progresses. For the answers, another function is called to pull the text and correct: status for each possible answer for the question.
-// It also creates individual buttons for each answer. There is also an "if" loop to 
+// This function is used to show the question in the container. It uses the questions array (further below) to change the question and answers each time the quiz progresses. For the answers, an arrow function created to pull the text and "correct:" status for each possible answer for the question.
+// It also creates individual buttons for each answer. There is also an "if" statement to validate if the answer selected is the correct answer. Once an answer button is clicked, the "selectAnswer" function will be called.
 
 function showQuestion(question) {
     questionElement.innerText = question.question;
@@ -89,20 +89,22 @@ function showQuestion(question) {
     })
 };
 
-//
+// This function is used to prevent the answer buttons from the HTML from populating in the container whenever the quiz is started or a question is answered, that way only the question and answers show in the container.
 
 function resetState() {
-    nextButton.classList.add('hide')
+    nextButtonElement.classList.add('hide');
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
-//
+// This function is used for when an answer is selected. If the selected answer is correct, the body of the page will be set to green and, if wrong, the body of the page will be set to red.
+// There is also an array created from each answer button that will set the "correct" status. If the answer is correct, 10 points will be added. If the answer is wrong, 15 seconds will be deducted. 
+// The last if-else statement determines if the quiz will keep populating questions or not. If the question answered is below the total question list length, a new question will be populated. Otherwise, the quiz will end, and the user will be brought to the score submission screen.
 
 function selectAnswer(event) {
-    const selectedButton = event.target;
-    const correct = selectedButton.dataset.correct;
+    const selectedButtonElement = event.target;
+    const correct = selectedButtonElement.dataset.correct;
     setStatusClass(document.body, correct);
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct);
@@ -121,58 +123,54 @@ function selectAnswer(event) {
     }
 };
 
-//
+// This function will show the final screen of the quiz. It will show the user their score and a textbox where they can add their initials to submit for the highscores page. Once the "submit" button is clicked, the user will be taken to the highscores page.
 
     function endGame () {
         endTextElement.classList.remove('hide');
-        finalScore.classList.remove('hide');
-        finalScore.innerText = "Your final score is "+ score + " points." ;
+        finalScoreElement.classList.remove('hide');
+        finalScoreElement.innerText = "Your final score is "+ score + " points." ;
         questionContainerElement.classList.add('hide');
-        submitButton.addEventListener('click', highScores);
+        submitButtonElement.addEventListener('click', highScores);
         
         
 }
 
-//
+// This function shows the highscores for the quiz. The scores for this page are set and pulled from the localstorage of the browser. The "ul" element is removed to prevent any unnecessary elements from showing on the page. Then, for each unique text input from the score submission, a unique key will be generated
+// that has the value (score) assigned with it. This key and value will then be pulled from the localstorage and displayed as a list item in the container.
 
 function highScores() {
-    console.log("click");
-    scores.classList.remove('hide');
+    
+    scoresElement.classList.remove('hide');
     endTextElement.classList.add('hide');
-    localStorage.setItem(inputText.value, JSON.stringify(score));
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild)
+    localStorage.setItem(inputTextElement.value, JSON.stringify(score));
+    while (ulElement.firstChild) {
+        ulElement.removeChild(ulElement.firstChild)
     }
     for (var i = 0, len = localStorage.length; i < len; ++i) {
         JSON.parse(localStorage.getItem(localStorage.key (i)));
-        console.log(localStorage.key (i));
-        console.log(localStorage.getItem(localStorage.key (i)));
         let liElement = document.createElement('li'); 
         liElement.innerText = localStorage.key (i) + " : " + localStorage.getItem(localStorage.key (i));
-        ul.append(liElement);   
-        var localStorageContents = JSON.parse(localStorage.getItem(localStorage.key (i)))
-        console.log(localStorageContents);
-        console.log(typeof(localStorageContents));
+        ulElement.append(liElement);   
     }  
    
-//    
+// This event listener will call the newGame function anytime the restart button is clicked.   
 
-    restartButton.addEventListener('click', newGame);  
+    restartButtonElement.addEventListener('click', newGame);  
 }
 
-//
+// This function will bring the user back to the original screen to start the quiz again, identical to if they would refresh the page.
 
 function newGame () {
     beginningTextElement.classList.remove('hide');
-    startButton.classList.remove('hide');
-    scores.classList.add('hide');
+    startButtonElement.classList.remove('hide');
+    scoresElement.classList.add('hide');
     endTextElement.classList.remove('hide');
     endTextElement.classList.add('hide');
-    finalScore.classList.add('hide');
-    inputText.value = "";
+    finalScoreElement.classList.add('hide');
+    inputTextElement.value = "";
 }
 
-//
+// These functions will call the clear the status, then reset and reassign the status of each answer button.
 
 function setStatusClass(element, correct) {
     clearStatusClass(element);
@@ -181,34 +179,50 @@ function setStatusClass(element, correct) {
     } else {
         element.classList.add('wrong')
     }
-};
-
-//
+}; 
 
 function clearStatusClass(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
 }
 
-//
+// This array stores the questions and answers, as well as the inner text and correct statuses for the answer buttons.
 
 const questions = [
     {
-        question: 'What is 4 + 4?',
+        question: 'What year was the very first model of the iPhone released?',
         answers: [
-           { text: '4', correct: false},
-           { text: '16', correct: false},
-           { text: '8', correct: true},
-           { text: '44', correct: false},
+           { text: '2010', correct: false},
+           { text: '2007', correct: true},
+           { text: '2006', correct: false},
+           { text: '2009', correct: false},
         ],
     },
     {
-        question: 'What is 2 x 4?',
+        question: 'What was Twitter’s original name?',
         answers: [
-           { text: '8', correct: true},
-           { text: '6', correct: false},
-           { text: '10', correct: false},
-           { text: '24', correct: false},
+           { text: 'twttr', correct: true},
+           { text: 'tweeter', correct: false},
+           { text: 'twister', correct: false},
+           { text: 'twang', correct: false},
+        ],
+    },
+    {
+        question: 'What part of the atom has no electric charge?',
+        answers: [
+           { text: 'Proton', correct: false},
+           { text: 'Muon', correct: false},
+           { text: 'Electron', correct: false},
+           { text: 'Neutron', correct: true},
+        ],
+    },
+    {
+        question: 'Which planet is the hottest in the solar system?',
+        answers: [
+           { text: 'Sun', correct: false},
+           { text: 'Mercury', correct: false},
+           { text: 'Venus', correct: true},
+           { text: 'Earth', correct: false},
         ],
     }
 ];
